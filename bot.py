62a8c5140daefadb9eed652ef38aea891a614e40
Ya Nan Sun <yanan@Yas-MacBook-Air.local>
2026-01-17 13:36:35 -0500

    were winning

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ class Bot:
     
     def find_highest_tile(self,spore, game_message, nutrient_grid: list[list[int]]) -> tuple[int]:
         max_nutrient = -1
-        min_position = (0, 0)
+        min_position = (-1, -1)
         min_distance = 10000000000
 
         for x in range(len(nutrient_grid)):
@@ -17,10 +17,20 @@ class Bot:
                 if game_message.world.ownershipGrid[x][y] == game_message.yourTeamId or nutrient_grid[x][y] == 0:
                     continue
                 distance = abs(spore.position.x - x) + abs(spore.position.y - y)
-                if distance < min_distance and distance > 0:
+                
+                if distance < min_distance and distance != 0:
                     min_distance = distance
                     min_position = (x, y)
-            
+        if min_position == (-1, -1):
+            for x in range(len(nutrient_grid)):
+                for y in range(len(nutrient_grid[0])):
+                    if game_message.world.ownershipGrid[x][y] == game_message.yourTeamId:
+                        continue
+                    distance = abs(spore.position.x - x) + abs(spore.position.y - y)
+                    if distance < min_distance and distance != 0:
+                        min_distance = distance
+                        min_position = (x, y)
+                
         return min_position
 
     def get_next_move(self, game_message: TeamGameState) -> list[Action]:
@@ -31,6 +41,8 @@ class Bot:
 
         my_team: TeamInfo = game_message.world.teamInfos[game_message.yourTeamId]
         if len(my_team.spawners) == 0:
+            actions.append(SporeCreateSpawnerAction(sporeId=my_team.spores[0].id))
+        elif len(my_team.spores) > 10:
             actions.append(SporeCreateSpawnerAction(sporeId=my_team.spores[0].id))
 
         elif len(my_team.spores) == 0:
@@ -43,10 +55,10 @@ class Bot:
                 SpawnerProduceSporeAction(spawnerId=my_team.spawners[0].id, biomass= 50)
             )
         elif my_team.nutrients >= 10:
-            actions.append(
-                SpawnerProduceSporeAction(spawnerId=my_team.spawners[0].id, biomass= int(0.5*my_team.nutrients))
+            for spawner in my_team.spawners:
+                actions.append(
+                SpawnerProduceSporeAction(spawnerId=spawner.id, biomass=20)
             )
-
 
         for spore in my_team.spores:
             actions.append(
